7d495d402c3d00214805cdd11a430e2c6cb7a3a9
b-smyers <bs602422@ohio.edu>
2024-10-18 13:08:11 -0400

    Added template game.py

---
+++ game.py
@@ -0,0 +1,30 @@
+# OUSDC Software Speedrun - Game Development Track
+# code template adapted from www.pygame.org/docs/
+import pygame
+
+# pygame setup
+pygame.init()
+running = True
+clock = pygame.time.Clock()
+dt = 0
+
+# game loop
+while running:
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # your code here
+
+
+
+    
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # calculate delta time (dt) for framerate independent events (ie. physics)
+    dt = clock.tick(60) / 1000 # cap at 60 fps
+
+pygame.quit()
